9bb1faf1e73787025e5005a69196d43207ed3ee5
Carelis Fagúndez <fagundez.carelis@gmail.com>
2025-11-25 21:26:56 -0300

    feat: conectar MainActivity a Firestore

--- app/src/main/java/com/example/misquehaceresapp/MainActivity.java
+++ app/src/main/java/com/example/misquehaceresapp/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializar Firestore
+        // Inicializar Firestore en la base
         db = FirebaseFirestore.getInstance();
 
         //mensaje de bienvenida
